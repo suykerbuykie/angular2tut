61836b9ad1d8ed8d7519994e59760283d4839116
Suijkerbuijk, TGP (ITCDECC) <tom.suijkerbuijk@klm.com>
2016-06-24 11:12:36 +0200

    introduced services, mock and promis.

---
+++ app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Suijkerbuijk' },
+  { id: 2, name: 'Verweij' },
+  { id: 3, name: 'Auf der Springe' },
+  { id: 4, name: 'Leu' },
+  { id: 5, name: 'Belchinov' },
+  { id: 6, name: 'Welp' },
+  { id: 7, name: 'Rori' },
+  { id: 8, name: 'Mir' },
+  { id: 9, name: 'Bakker' },
+  { id: 10, name: 'Camera' },
+  { id: 11, name: 'Heere' },
+  { id: 12, name: 'Okon' }
+];
