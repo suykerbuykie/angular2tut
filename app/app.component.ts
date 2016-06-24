acda4ecd5922e784368f99325b7f75d78181818c
Suijkerbuijk, TGP (ITCDECC) <tom.suijkerbuijk@klm.com>
2016-06-24 09:39:13 +0200

    finished first chapter of tut

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,15 +7,16 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2><
-        div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input value="{{hero.name}}" placeholder="name">
-        </div>
-        `
+    template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
+
 })
 
 export class AppComponent {
