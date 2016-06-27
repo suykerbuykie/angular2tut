d55dc2c023355f37b1791ca2f2a25997774d8c8d
Suijkerbuijk, TGP (ITCDECC) <tom.suijkerbuijk@klm.com>
2016-06-27 09:51:53 +0200

    styles and routes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Router } from '@angular/router-deprecated';
+
 @Injectable()
 
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
-  }
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number) {
+        return this.getHeroes()
+             .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+    }
 }
